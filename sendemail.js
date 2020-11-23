394fb625205bae10603f799116fba9a4a42ccdf2
LeeviSvard <leevi.svard@edu.salpaus.fi>
2020-11-23 12:03:06 +0000

    new

--- sendemail.js
+++ sendemail.js
@@ -1,16 +1,17 @@
+
 const emailnappi = document.querySelector('.lahetanappi');
 
 console.log (emailnappi);
 emailnappi.addEventListener('click', e=> {
 e.preventDefault();
-sendJSON(); 
+  sendJSON(); 
 });
 
 function sendJSON(){
   let zhr = new XMLHttpRequest();
   let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp1?code=lWOELqiU07AqsBviOQYzuNIrQP7xoV7NV7C5W2ctgjIRcf7nXE2biw==";
 
-xhr.open('post', url, true);
+xhr.open('POST', url, true);
 
 xhr.setRequestHeader("Content-Type", "application/json"); 
 }
@@ -19,12 +20,12 @@ xhr.setRequestHeader("Content-Type", "application/json");
     if (xhr.readyState === 4 && xhr.status == 200) {
     console.log ("valmis, yhteys toimii");
     }
-};
-var data = JSON.stringify({
-    "EmailMsg": "Azure"
-    "EmailAddress": "mira.vorne@salpaus.fi",
-    "EmailTo": "leevi.svard@gmail.com",
-    "EmailName": "Tyyppi Testaa",
-});
- xhr.send(data); 
-}
+
+    var data = JSON.stringify({
+      "EmailMsg": "viesti",
+      "EmailAddress": "formtrigger@salpaus.fi",
+      "EmailTo": "leevi.svard@gmail.com",
+      "EmailName": "Tyyppi Testaa"
+    }); 
+    xhr.send(data); 
+  } 
