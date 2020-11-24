06ebcc7b46d4d03ceb12109e952bed37bd3f6d26
LeeviSvard <leevi.svard@edu.salpaus.fi>
2020-11-24 08:07:21 +0000

    dd

--- sendemail.js
+++ sendemail.js
@@ -7,7 +7,7 @@ emailnappi.addEventListener('click', e => {
 });
 
   function sendJSON(){
-    let zhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp1?code=lWOELqiU07AqsBviOQYzuNIrQP7xoV7NV7C5W2ctgjIRcf7nXE2biw==";
     xhr.open('POST', url, true);
 
